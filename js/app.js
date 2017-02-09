fa3b0f28ffb2b14576a3c673a5e6d66a1271e8af
chuqian <icey_chu@163.com>
2017-02-09 14:42:47 +0800

    修成本控制单货币格式和对齐方式

--- js/app.js
+++ js/app.js
@@ -16,8 +16,8 @@ var app = angular.module("designer", ["ui.router", 'angular-loading-bar','ngDial
 var g = {
 	
 	// host server
-	host: 'http://192.168.0.87',
-	// host: 'http://192.168.0.6:8080',
+	// host: 'http://192.168.0.87',
+	host: 'http://192.168.0.6:8080',
 	// host: 'http://192.168.0.224:8089',
 
 	// user: {
